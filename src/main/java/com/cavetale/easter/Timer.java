83681d908638d9fc10e453b0b960df8a6ead127a
StarTux <ths.loy@googlemail.com>
2023-04-08 00:01:42 +0200

    Extend the period

--- src/main/java/com/cavetale/easter/Timer.java
+++ src/main/java/com/cavetale/easter/Timer.java
@@ -15,7 +15,7 @@ public final class Timer {
     @Getter private static int hour;
     @Getter private static int dayOfWeek;
     public static final int ONE = 20230407;
-    public static final int END = 20230421;
+    public static final int END = 20230424;
     public static final int DAYS = END - ONE + 1;
     public static final int EGGS_PER_DAY = 7;
 
