ad882a2bde4261f7579c0f6aec6697808801f7da
StarTux <ths.loy@gmail.com>
2024-03-31 14:02:01 +0200

    Extend to 2 weeks; fix the buggy adhocery

--- src/main/java/com/cavetale/easter/Timer.java
+++ src/main/java/com/cavetale/easter/Timer.java
@@ -4,8 +4,9 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import org.bukkit.Bukkit;
+import java.util.Arrays;
 import lombok.Getter;
+import org.bukkit.Bukkit;
 
 public final class Timer {
     @Getter private static int dayId;
@@ -15,15 +16,33 @@ public final class Timer {
     @Getter private static int hour;
     @Getter private static int dayOfWeek;
     public static final int ONE = 20240324;
-    public static final int END = 20240331;
-    public static final int DAYS = END - ONE + 1;
+    public static final int END = 20240407;
+    private static final int[] EASTER_DAYS = {
+        ONE,
+        20240325,
+        20240326,
+        20240327,
+        20240328,
+        20240329,
+        20240330,
+        20240331,
+        20240401,
+        20240402,
+        20240403,
+        20240404,
+        20240405,
+        20240406,
+        END,
+    };
+    public static final int DAYS = EASTER_DAYS.length;
     public static final int EGGS_PER_DAY = 7;
+    public static final ZoneId ZONE_ID = ZoneId.of("UTC-11");
 
     private Timer() { }
 
     static void update() {
         Instant instant = Instant.now();
-        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.of("UTC-11"));
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZONE_ID);
         LocalDate localDate = localDateTime.toLocalDate();
         year = localDate.getYear();
         month = localDate.getMonth().getValue();
@@ -44,15 +63,14 @@ public final class Timer {
      */
     public static int getEasterDay() {
         if (dayId < ONE || dayId > END) return 0;
-        return dayId - ONE + 1;
+        return Arrays.binarySearch(EASTER_DAYS, dayId) + 1;
     }
 
     /**
      * How many regular eggs a player can find right now.
-     * Basically amount of days times eggs per day, except the final
-     * day is considered overhang and does not count.
+     * Basically amount of days times eggs per day.
      */
     public static int getTotalEggs() {
-        return Math.min(DAYS - 1, getEasterDay()) * EGGS_PER_DAY;
+        return Math.min(DAYS, getEasterDay()) * EGGS_PER_DAY;
     }
 }
